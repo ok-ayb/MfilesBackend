9bc4e55a786afc34e7a8a1981a45fedf591c16d1
MariamELAYADI <mariam_elayadi@hotmail.com>
2023-03-20 13:59:24 +0100

    initial project setup

---
+++ src/main/java/smwall/smbackend/SmBackendApplication.java
@@ -0,0 +1,13 @@
+package smwall.smbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/smwall/smbackend/SmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package smwall.smbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
