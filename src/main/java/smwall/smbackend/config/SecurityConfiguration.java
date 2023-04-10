255a9f535543d19612dc844fb8ed9152e793e106
Hassan Ait Nacer <itshassanaitnacer@gmail.com>
2023-04-10 13:40:02 +0000

    feat[SMW-60]: Configure the Security Configuration to allow webhooks endpoints

--- src/main/java/smwall/smbackend/config/SecurityConfiguration.java
+++ src/main/java/smwall/smbackend/config/SecurityConfiguration.java
@@ -1,2 +1,30 @@
-package smwall.smbackend.config;public class SecurityConfiguration {
+package smwall.smbackend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import smwall.smbackend.holders.ApiPaths;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers(ApiPaths.V1 + ApiPaths.WEBHOOKS + "/**").permitAll()
+                .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
 }
