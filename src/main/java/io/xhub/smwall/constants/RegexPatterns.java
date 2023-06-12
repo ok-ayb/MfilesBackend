34e79a93d79f777ff7333f760024115019aedc85
Redwane Ait Oukazzamane <redwane.aitoukazzamane@x-hub.io>
2023-06-12 14:56:58 +0000

    fix[SMW-205]: Regex title to verify

--- src/main/java/io/xhub/smwall/constants/RegexPatterns.java
+++ src/main/java/io/xhub/smwall/constants/RegexPatterns.java
@@ -4,7 +4,7 @@ public class RegexPatterns {
     public final static String SOCIAL_MEDIA_HASHTAG = "#[^ !@#$%^&*(),.?\":{}|<>]*";
     public final static String USER_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
     public final static String SOURCE_TYPES = "(?:^|\\s+)(?:(?<mention>@)|(?<hash>#))(?<item>\\w+)(?=\\s*)";
-    public static final String WALL_TITLE = "\\w[\\w.\\-#&\\s]{4,}";
+    public static final String WALL_TITLE = "^[\\s\\S]{5,}$";
     public final static String ANNOUNCEMENT_TITLE = "^[a-zA-Z0-9'\\-]{5,}$";
     public final static String ANNOUNCEMENT_DESCRIPTION = "^(.{0,200})$";
 }
