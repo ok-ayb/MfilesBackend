eb5053b6a7b7ebbc399db14c16b36efda571bd59
Ayoub Ouakki <ayoub.ouakki@x-hub.io>
2023-07-07 11:05:49 +0000

    feat[SMW-260] : Update Announcement Test

--- src/test/java/io/xhub/smwall/service/AnnouncementServiceTest.java
+++ src/test/java/io/xhub/smwall/service/AnnouncementServiceTest.java
@@ -1,11 +1,13 @@
 package io.xhub.smwall.service;
 
 import io.xhub.smwall.commands.AnnouncementAddCommand;
+import io.xhub.smwall.commands.AnnouncementUpdateCommand;
 import io.xhub.smwall.domains.Announcement;
 import io.xhub.smwall.events.announcement.AnnouncementCreatedEvent;
 import io.xhub.smwall.exceptions.BusinessException;
 import io.xhub.smwall.repositories.AnnouncementRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -14,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
 
 import java.time.Instant;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -28,6 +31,7 @@ class AnnouncementServiceTest {
     @InjectMocks
     private AnnouncementService announcementService;
     private AnnouncementAddCommand addCommand;
+    private AnnouncementUpdateCommand updateCommand;
 
     @BeforeEach
     void setUp() {
@@ -36,7 +40,13 @@ class AnnouncementServiceTest {
                 "This is a test announcement description",
                 Instant.now().plusSeconds(3600),
                 Instant.now().plusSeconds(3700));
+        updateCommand = new AnnouncementUpdateCommand(
+                "New Title",
+                "New Description",
+                Instant.now().plusSeconds(3800),
+                Instant.now().plusSeconds(3900));
     }
+
 
     @Test
     void should_addAnnouncement_when_AnnouncementIsValid() {
@@ -72,6 +82,26 @@ class AnnouncementServiceTest {
             announcementService.addAnnouncement(addCommand);
         });
     }
+    @Test
+    void should_updateAnnouncement_when_AnnouncementIsValid() {
+        Announcement announcement = new Announcement();
+        announcement.setId("announcementId");
+        announcement.setTitle("Old Title");
+        announcement.setDescription("Old Description");
+        announcement.setStartDate(Instant.now().plusSeconds(3600));
+        announcement.setEndDate(Instant.now().plusSeconds(3700));
 
+        when(announcementRepository.findById(announcement.getId())).thenReturn(Optional.of(announcement));
+        Announcement updatedAnnouncement = announcementService.updateAnnouncement(announcement.getId(),updateCommand);
+
+        assertNotNull(updatedAnnouncement);
+        assertEquals(announcement.getId(), updatedAnnouncement.getId());
+        assertEquals(updateCommand.getTitle(), updatedAnnouncement.getTitle());
+        assertEquals(updateCommand.getDescription(), updatedAnnouncement.getDescription());
+        assertEquals(updateCommand.getStartDate(), updatedAnnouncement.getStartDate());
+        assertEquals(updateCommand.getEndDate(), updatedAnnouncement.getEndDate());
+
+        verify(announcementRepository, times(1)).findById(announcement.getId());
+    }
 
 }
